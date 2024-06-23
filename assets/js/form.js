aeed85d226182b4858728dde0e4c734ac1df2c52
philcurtis422 <philcurtis422@gmail.com>
2024-06-23 19:37:23 -0400

    added dark mode to main page

---
+++ assets/js/form.js
@@ -0,0 +1,11 @@
+/*grab form by query */
+
+const form = document.querySelector('form');
+
+/* function that collects the data and saves it as an object in an array*/
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    
+});
---
+++ assets/js/logic.js
@@ -0,0 +1,39 @@
+/* creat darkmode */
+
+const darkModeButton = document.querySelector('#dark-mode-button');
+const headerClass = document.querySelector('header');
+const sectionClass = document.querySelector('section');
+const submitClass = document.querySelector('.submit');
+
+console.log(darkModeButton.textContent);
+let darkMode = function () {
+   
+    //changes to the styles
+    darkModeButton.classList.toggle('darkMode');
+    headerClass.classList.toggle('darkMode');
+    sectionClass.classList.toggle('darkMode');
+    submitClass.classList.toggle('darkMode');
+
+
+    //change the dark mode button to light mode
+    if (darkModeButton.textContent === 'Dark Mode') {
+        darkModeButton.innerHTML = 'Light Mode';
+       console.log(darkModeButton.textContent)
+    } else {
+        darkModeButton.innerHTML = 'Dark Mode';
+        
+    }
+
+    
+
+    const labels = [...document.querySelectorAll('label')];
+
+               for(let i = 0; i < labels.length; i++){
+                  labels[i].classList.toggle('darkMode');
+              }
+}
+
+darkModeButton.addEventListener('click', darkMode);
+
+
+
